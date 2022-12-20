1605d4a833f8e33d6b3e099b6b899bc249f75168
simsim2002 <110578986+simsim2002@users.noreply.github.com>
2022-12-20 09:52:12 +0700

    Initial commit

---
+++ simsim2002/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h1>{{name}}</h1>
+  <h1>{{firstname}}</h1>
+  <h1>{{lastname}}</h1>
+  <h1>{{age}}</h1>
+  <h1>{{tel}}</h1>
+  <h1>{{room}}</h1>
+  <h1>{{major}}</h1>
+  <h1>{{school}}</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      name:"sim",
+      firstname:"NATTHAPOL",
+      lastname:"THANYACHAROEN",
+      age:"20",
+      tel:"174",
+      room:"IT5102",
+      major:"information technology",
+      school:"lannapoly technical college"
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
