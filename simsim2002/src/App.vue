ed6b1afa087c2b8d4d52ebffb31d6bd31a5ae304
simsim2002 <110578986+simsim2002@users.noreply.github.com>
2022-12-20 10:49:07 +0700

    0

--- simsim2002/src/App.vue
+++ simsim2002/src/App.vue
@@ -1,13 +1,31 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <h1>{{name}}</h1>
-  <h1>{{firstname}}</h1>
-  <h1>{{lastname}}</h1>
+  <h1>{{firstname}} &nbsp;{{lastname}}</h1>
+  <h1>{{ getFullname() }}</h1>
   <h1>{{age}}</h1>
   <h1>{{tel}}</h1>
   <h1>{{room}}</h1>
   <h1>{{major}}</h1>
   <h1>{{school}}</h1>
+<h2>{{ pofile[gender] }}</h2>
+<ul>
+  <li>
+    {{ hobby[0] }}
+  </li>
+  <li>
+    {{ hobby[1] }}
+  </li>
+  <li>
+    {{ hobby[2] }}
+  </li>
+  <li>
+    {{ hobby[3] }}
+  </li>
+  <li>
+    {{ hobby[4] }}
+  </li>
+</ul>
 </template>
 
 <script>
@@ -23,8 +41,16 @@ export default {
       tel:"174",
       room:"IT5102",
       major:"information technology",
-      school:"lannapoly technical college"
+      school:"lannapoly technical college",
+      hobby:["ดูหนัง","ฟังเพลง","เล่นเกม","ต่อกันดั้ม","นอน"],
+      pofile:{ gender:"mile",weight:50,hight:170, statud:false}
     }
+  },
+  methods:{
+    getFullname(){
+      return this.firstname + this.lastname
+    }
+
   }
 }
 </script>
